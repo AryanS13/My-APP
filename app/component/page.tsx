3ae9e6d6bbbb41799e810dedea55a14e2352e56e
Aryan <aroonba13thknight@gmail.com>
2023-08-10 00:53:20 +0530

    minor fix

--- app/component/page.tsx
+++ app/component/page.tsx
@@ -1,11 +1,17 @@
-async function getData() {
-    const data = await fetch('https://dummy.restapiexample.com/api/v1/employee/1');
-    const jsonData = await data.json();
-    return jsonData;
-  }
+// async function getData() {
+//     const data = await fetch('https://dummy.restapiexample.com/api/v1/employee/1');
+//     const jsonData = await data.json();
+//     return jsonData;
+//   }
 
 export default async function MyComponent() {
-    const myData = await getData();
+    // const myData = await getData();
+    const myData = {
+        "data": {
+            "employee_name": "Aryan",
+            "employee_salary": 10000
+        }
+    }
 
     return (
       <>
