70b1fe24a2e431be5e1660c751b6a36e0f23d608
Aryan <aroonba13thknight@gmail.com>
2023-08-10 10:19:38 +0530

    amplify testing

--- app/component/page.tsx
+++ app/component/page.tsx
@@ -1,23 +1,19 @@
-// async function getData() {
-//     const data = await fetch('https://dummy.restapiexample.com/api/v1/employee/1');
-//     const jsonData = await data.json();
-//     return jsonData;
-//   }
+async function getData() {
+    const data = await fetch('https://dummy.restapiexample.com/api/v1/employee/1');
+    const jsonData = await data.json();
+    return jsonData;
+  }
 
 export default async function MyComponent() {
-    // const myData = await getData();
-    const myData = {
-        "data": {
-            "employee_name": "Aryan",
-            "employee_salary": 10000
-        }
-    }
+    const myData = await getData();
+    console.log(typeof(myData));
+    console.log(myData);
 
     return (
       <>
       <h1>renderring</h1>
-        <div>{myData.data.employee_name}</div>
-        <div>{myData.data.employee_salary}</div>
+        <div>{myData?.data.employee_name}</div>
+        <div>{myData?.data.employee_salary}</div>
       </>
     )
   }
